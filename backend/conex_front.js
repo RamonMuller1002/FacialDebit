f395269a75ffce17e812af4f05c40b595d353094
RamonMuller1002 <ramonmuller1002@gmail.com>
2025-10-20 08:43:42 -0300

    updating front

---
+++ backend/conex_front.js
@@ -0,0 +1,5 @@
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const confirmPassword = document.getElementById('confirmPassword')
+const birthDate = document.getElementById('birthDate')
